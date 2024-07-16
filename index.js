34b12ef2ff6d6cd19beea45b7e9cdc3335381bfd
Mikey191 <desertigorb@gmail.com>
2024-07-16 17:44:37 +0300

    error handler/typeController/brandController/deviceController(create device whith img)

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ const express = require("express");
 const sequelize = require("./db");
 const models = require("./models/models");
 const cors = require("cors");
+const fileUpload = require("express-fileupload");
 const router = require("./routes/index");
+const errorHandler = require("./middleware/ErrorHandingMiddleware");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload({}));
 app.use("/api", router);
+
+// Обработка ошибок. Последний Middleware
+app.use(errorHandler);
 
 const start = async () => {
   try {

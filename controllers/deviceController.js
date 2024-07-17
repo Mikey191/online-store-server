a8c9874b808c81aa34a66e63a4bfa78bdb35ec57
Mikey191 <desertigorb@gmail.com>
2024-07-17 11:37:25 +0300

    static files, get all device, get one device, pagination

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -1,6 +1,6 @@
 const uuid = require("uuid");
 const path = require("path");
-const { Device } = require("../models/models");
+const { Device, DeviceInfo } = require("../models/models");
 const ApiError = require("../error/ApiError");
 
 class DeviceController {
@@ -19,13 +19,62 @@ class DeviceController {
         img: fileName,
       });
 
+      if (info) {
+        info = JSON.parse(info);
+        info.forEach((i) =>
+          DeviceInfo.create({
+            title: i.title,
+            description: i.description,
+            deviceId: device.id,
+          })
+        );
+      }
+
       return res.json(device);
     } catch (error) {
-      next(ApiError.badRequest(error.message))
+      next(ApiError.badRequest(error.message));
     }
   }
-  async getAll(req, res) {}
-  async getOne(req, res) {}
+  async getAll(req, res) {
+    let { brandId, typeId, limit, page } = req.query;
+    page = page || 1;
+    limit = limit || 9;
+    let offset = page * limit - limit;
+    let devices;
+    if (!brandId && !typeId) {
+      devices = await Device.findAndCountAll({ limit, offset });
+    }
+    if (brandId && !typeId) {
+      devices = await Device.findAndCountAll({
+        where: { brandId },
+        limit,
+        offset,
+      });
+    }
+    if (!brandId && typeId) {
+      devices = await Device.findAndCountAll({
+        where: { typeId },
+        limit,
+        offset,
+      });
+    }
+    if (brandId && typeId) {
+      devices = await Device.findAndCountAll({
+        where: { typeId, brandId },
+        limit,
+        offset,
+      });
+    }
+    return res.json(devices);
+  }
+  async getOne(req, res) {
+    const { id } = req.params;
+    const device = await Device.findOne({
+      where: { id },
+      include: [{ model: DeviceInfo, as: "info" }],
+    });
+    return res.json(device);
+  }
 }
 
 module.exports = new DeviceController();

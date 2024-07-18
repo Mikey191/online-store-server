748c31c4c532633b1b426ebc9976771495473f30
Mikey191 <desertigorb@gmail.com>
2024-07-18 11:21:55 +0300

    middleware authorization

--- controllers/userController.js
+++ controllers/userController.js
@@ -29,24 +29,21 @@ class UserController {
     return res.json({ token });
   }
   async login(req, res, next) {
-    const {email, password} = req.body
-    const user = await User.findOne({where: {email}})
-    if(!user){
-      return next(ApiError.internal("Пользователь с таким mail не найден"))
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return next(ApiError.internal("Пользователь с таким mail не найден"));
     }
-    let comparePassword = bcrypt.compareSync(password, user.password)
-    if(!comparePassword){
-      return next(ApiError.internal("Указан неверный пароль"))
+    let comparePassword = bcrypt.compareSync(password, user.password);
+    if (!comparePassword) {
+      return next(ApiError.internal("Указан неверный пароль"));
     }
-    const token = generateJwt(user.id, user.email, user.role)
-    return res.json({token})
+    const token = generateJwt(user.id, user.email, user.role);
+    return res.json({ token });
   }
   async check(req, res, next) {
-    const { id } = req.query;
-    if (!id) {
-      return next(ApiError.badRequest("Не задан ID"));
-    }
-    res.json(id);
+    const token = generateJwt(req.user.id, req.user.email, req.user.role);
+    return res.json({ token });
   }
 }
 

403ef2d2651e94ccdb089b310afd657e94875ec5
Mikey191 <desertigorb@gmail.com>
2024-07-31 14:10:31 +0300

    device create debug

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -6,7 +6,7 @@ const ApiError = require("../error/ApiError");
 class DeviceController {
   async create(req, res, next) {
     try {
-      const { name, price, brandId, typeId, info } = req.body;
+      let { name, price, brandId, typeId, info } = req.body;
       const { img } = req.files;
       let fileName = uuid.v4() + ".jpg";
       img.mv(path.resolve(__dirname, "..", "static", fileName));

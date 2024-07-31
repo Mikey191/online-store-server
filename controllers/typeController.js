f9802a45f9f1ca24398fb9db3670ce0fb70cc668
Mikey191 <desertigorb@gmail.com>
2024-07-31 11:36:19 +0300

    type create trycatch

--- controllers/typeController.js
+++ controllers/typeController.js
@@ -2,10 +2,14 @@ const { Type } = require("../models/models");
 const ApiError = require("../error/ApiError");
 
 class TypeController {
-  async create(req, res) {
-    const { name } = req.body;
-    const type = await Type.create({ name });
-    return res.json(type);
+  async create(req, res, next) {
+    try {
+      const { name } = req.body;
+      const type = await Type.create({ name });
+      return res.json(type);
+    } catch (error) {
+      next(ApiError.badRequest(error.message));
+    }
   }
   async getAll(req, res) {
     const types = await Type.findAll();
